43e7e644ea1e48f7a631024a42fd4718c54073b5
Dilmika Sahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-09-11 12:40:11 +0530

    Delete main directory

--- main/java/com/employeeMS/employeeMS/repository/employeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.employeeMS.employeeMS.repository;
-
-import com.employeeMS.employeeMS.entity.employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface employeeRepo extends JpaRepository<employee,Integer> {
-}
